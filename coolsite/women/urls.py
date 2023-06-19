a3cfcb2958d5499b18f06dc8d46114d497e26ad3
timofey-davidov <timofey_davidov_95@mail.ru>
2023-06-19 03:07:54 +0300

    finished Templates

--- coolsite/women/urls.py
+++ coolsite/women/urls.py
@@ -4,6 +4,5 @@ from women.views import *
 
 urlpatterns = [
     path('', index, name="home"),
-    path('cats/<slug:cat>/', categories),
-    path('archive/<int:year>/', archive),
+    path('about/', about, name="about"),
 ]
